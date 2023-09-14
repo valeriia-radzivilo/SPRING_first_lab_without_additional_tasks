217e9323b36f5be3f7c2633f0a8142988003b7d2
Valeriia Radzivilo <v.radzivilo.ip14@kpi.ua>
2023-09-14 22:08:35 +0300

    Additions to make the 1st lab task, using beans and order

--- src/main/java/com/sping/first_lab/FirstLabApplication.java
+++ src/main/java/com/sping/first_lab/FirstLabApplication.java
@@ -1,13 +1,33 @@
 package com.sping.first_lab;
 
+import com.sping.first_lab.beans.Config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
-public class FirstLabApplication {
+public class FirstLabApplication implements CommandLineRunner {
+    @Autowired
+    private List<String> beanList;
+    public static void main(String[] args) {
+System.out.println("Begin of main :");
+        SpringApplication.run(FirstLabApplication.class, args);
+        System.out.println("End of main");
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(FirstLabApplication.class, args);
-	}
+    @Override
+    public void run (String... args) throws Exception{
+         System.out.println(beanList.get(0));
+        System.out.println("Hello from spring boot!");
+        System.out.println(beanList.get(1));
+    }
+
+
 
 }
--- src/main/java/com/sping/first_lab/beans/Config.java
+++ src/main/java/com/sping/first_lab/beans/Config.java
@@ -1,2 +1,22 @@
-package com.sping.first_lab.beans;public class config {
+package com.sping.first_lab.beans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Component
+public class Config {
+    @Bean("first")
+    @Order(1)
+    public String getFirstBean() {
+        return "First";
+    }
+    @Bean("second")
+    @Order(2)
+    public String getSecondBean() {
+        return "Second";
+    }
 }
