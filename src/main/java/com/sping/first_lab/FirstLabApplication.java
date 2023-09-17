609309d3188ce2a5a679c4f7b74be8c6f566a64f
Valeriia Radzivilo <v.radzivilo.ip14@kpi.ua>
2023-09-17 11:27:51 +0300

    Deleting additional tasks

--- src/main/java/com/sping/first_lab/FirstLabApplication.java
+++ src/main/java/com/sping/first_lab/FirstLabApplication.java
@@ -1,20 +1,13 @@
 package com.sping.first_lab;
 
-import com.sping.first_lab.beans.Config;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 
-import java.util.List;
 
 @SpringBootApplication
 public class FirstLabApplication implements CommandLineRunner {
-    @Autowired
-    private List<String> beanList;
     public static void main(String[] args) {
         System.out.println("Begin of main :");
         SpringApplication.run(FirstLabApplication.class, args);
@@ -23,9 +16,9 @@ public class FirstLabApplication implements CommandLineRunner {
 
     @Override
     public void run (String... args) throws Exception{
-        System.out.println(beanList.get(0));
+
         System.out.println("Hello from spring boot!");
-        System.out.println(beanList.get(1));
+
     }
 
 
